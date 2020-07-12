f81829b94098481b21af78df5401d0b6a0cd18ed
Narayan Krishna <nkrishn8@gmail.com>
2020-07-12 13:12:50 +0530

    Important Libraries imported in Intr0_to_Dl

---
+++ Intro_Deep_Learning.py
@@ -0,0 +1,9 @@
+'''
+Introduction To Deep Learning with Python.
+'''
+# Importing Libraries
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
