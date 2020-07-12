91f14bc4fe9e7b615d2782182a57e34a9b1bbc4c
Narayan Krishna <nkrishn8@gmail.com>
2020-07-12 13:23:22 +0530

    fashion dataset loaded

--- Intro_Deep_Learning.py
+++ Intro_Deep_Learning.py
@@ -1,9 +1,21 @@
 '''
 Introduction To Deep Learning with Python.
 '''
+# Prediction of clothes through images (shirt, shoes etc).
+
 # Importing Libraries
 
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Loading data(fashion includes different clothes) from keras
+
+data = keras.datasets.fashion_mnist
+
+# Splitting the images and labels into train and test data.
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+print(train_label[0])
+# Adding labels in list so that it can be easilyi identified later
