36e4390e694dc59170ef5c132be5394bd3954e7f
Narayan Krishna <nkrishn8@gmail.com>
2020-07-14 15:26:36 +0530

    model saved

--- text_classification.py
+++ text_classification.py
@@ -13,7 +13,7 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words = 880000 ) # Split 10000 words
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words = 88000 ) # Split 88000 words
 
 # Getting word_index
 
@@ -21,7 +21,7 @@ word_index = data.get_word_index()
 
 word_index = {k:(v+3) for k,v in word_index.items()}
 
-print(word_index[0])
+
 
 word_index['<PAD>'] = 0 # Creating Padding Word Tag, all comments have different length so we give padding tag so that each and every comment have same length
 word_index['<START>'] = 1 # Creating Start Word Tag
@@ -33,13 +33,13 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 # Preprocessing of data
 
-# Allowing 150 words in comments
+# Allowing 250 words in comments
 
 train_data = keras.preprocessing.sequence.pad_sequences(train_data, value = word_index['<PAD>'], padding ='post', maxlen = 250)
 test_data = keras.preprocessing.sequence.pad_sequences(test_data, value = word_index['<PAD>'], padding ='post', maxlen = 250)
 
 # Function For making prediction readable return text comment
-def decode_review(test):
+def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
 print(decode_review(test_data[0]))
@@ -50,10 +50,18 @@ model = keras.Sequential()
 model.add(keras.layers.Embedding(88000 ,16)) # This layer is responsible for finding vectors of each word
 model.add(keras.layers.GlobalAveragePooling1D()) # Convert into average for each layer
 model.add(keras.layers.Dense(16, activation = 'relu')) # To recognize pattern
-model.add(keras.layers.Dense(16, activation = 'sigmoid'))
+model.add(keras.layers.Dense(1, activation = 'sigmoid'))
 
 model.summary()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Saving Model
+
+model.save('imdb_model.h5')
+
+# Loading the model
+
+#model = keras.models.load_model('imdb_model.h5')
 
 # Splitting the training data into validation and training data
 
@@ -61,7 +69,7 @@ x_val = train_data[:10000]
 x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
-y_train = train_labels[:10000]
+y_train = train_labels[10000:]
 
 # Model Fitting
 
