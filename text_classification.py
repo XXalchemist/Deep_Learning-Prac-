1e14f316e68bac63b4844e966cc99b7b7feae98f
Narayan Krishna <nkrishn8@gmail.com>
2020-07-14 14:29:39 +0530

    Evaluation of test_data

--- text_classification.py
+++ text_classification.py
@@ -62,3 +62,12 @@ x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
 y_train = train_labels[:10000]
+
+# Model Fitting
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data = (x_val, y_val), verbose=1)
+
+# Evaluation of test_data
+
+results = model.evaluate(test_data, test_labels)
+print(results)
